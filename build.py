fb359fcdc65d433d0fa4e0d41799e500f6b64261
matan h <matan.honig2@gmail.com>
2021-06-25 11:56:34 +0300

    update build.py

--- build.py
+++ build.py
@@ -1,76 +1,47 @@
 import os
 import subprocess
+import platform
 import sys
 import time
-import logging
-import platform
 
 platform = platform.system()
-sys.excepthook = lambda *args: logging.exception("Uncaught exception", exc_info=args)
 
+file = "main.py"
 
-def error(msg, *args, exc_info=True, **kwargs):
-    """logging.exception(error) and then exit"""
-    if exc_info and not (sys.exc_info()):
-        exc_info = False
-    logging.exception(msg, *args, exc_info, **kwargs)
-    quit(1)
-
-
-############
-
-file = 'main.py'
 if platform == "Windows":
-    exe_file = 'snippets.exe'
+    ext = '.exe'
+elif platform == "Darwin":
+    ext = ".app"
 else:
-    exe_file = "snippets.bin"
-add = lambda s, l: list(map(lambda x: s + x, l))
+    ext = ".bin"
+
+exe_file = os.path.join("dist", "snippets") + ext
 
 
 def run(*args):
-    command = ' '.join(args)
-    return os.system(command)
+    args = " ".join(args)
+    subprocess.check_call(args, shell=True)
 
 
 def compile_exe(debug=False):
-    plugins = ['multiprocessing', 'tk-inter', 'pylint-warnings']
-
-    options = [
-        # one file,standalone:
-        'onefile',
-        'windows-onefile-tempdir',
-        'standalone' if not debug else None,
-        # without console
-        'windows-disable-console' if not debug else None,
-        # Removes the build directory after producing
-        'remove-output',
-        'windows-icon-from-ico=icon.ico',
-        'linux-onefile-icon=icon.ico'
-        # todo:set icon with --windows-icon-from-ico and --linux-onefile-icon)
-    ]
-    options = list(filter(None, options))
-
-    plugins = add('--plugin-enable=', plugins)
-    options = add('--', options)
-
-    # plugins = list(map(lambda x: '--plugin-enable=' + x, plugins))
-    return run(
-        "python -m nuitka",
-        *options,
-        *plugins,
-        "-o " + exe_file,
-        file,
+    run(
+        "pyinstaller",
+        "--onefile",
+        "--windowed" if not debug else '',
+        "--icon icon.ico",
+        "--name snippets",
+        "--add-data icon.ico",
+        "main.py"
     )
 
 
 def valid():
-    if compile_exe(debug=True) != 0:
-        error("error in debug compiling")
+    compile_exe(debug=True)
     p = subprocess.Popen(exe_file)
     time.sleep(4)
     if p.poll() is not None:
         if p.returncode != 0:
-            error(f"error when run snippets.exe - {p.returncode}")
+            raise Exception(f"error when run snippets.exe - {p.returncode}")
     p.kill()
     print("done debug")
 
@@ -81,7 +52,13 @@ if __name__ == '__main__':
         if a[0].lower().strip() == "debug":
             valid()
 
-    if compile_exe() != 0:
-        error('error in normal compiling')
+    compile_exe()
+
+    if os.path.exists(exe_file):
+        print("exe_file:", exe_file)
+    else:
+        print("exe_file not found...", "here is dir:", os.listdir())
+        if os.path.exists("dist"):
+            print("dir of dist:", os.listdir("dist"))
 
     print("done")
