de0fe4d5b9e7d6ff4849cd5b84b98e37c298754e
matan h <matan.honig2@gmail.com>
2021-07-19 09:51:27 +0300

    update build-setup.py

--- build-setup.py
+++ build-setup.py
@@ -1,9 +1,9 @@
-from os import path, system
-
+from os import path
+from subprocess import check_call
 for p in (
 path.expandvars("%LOCALAPPDATA%\Programs"),
 path.expandvars("%ProgramFiles(x86)%"),
 path.expandvars("%ProgramFiles%")):
     if path.exists(f"{p}\Inno Setup 6\iscc.exe"):
-        system(f"{p}\Inno Setup 6\iscc.exe setup.iss")
+        check_call([f"{p}\Inno Setup 6\iscc.exe","setup.iss"])
         break
